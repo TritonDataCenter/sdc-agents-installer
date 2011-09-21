7c7f9b3917d8ab77aeef71e5dc7723e9b0a31907
Orlando Vazquez <ovazquez@gmail.com>
2011-09-21 11:46:27 -0700

    AGENT-185 AGENT-309: Refactor agent upgrades, make the script idempotent

--- install.sh
+++ install.sh
@@ -6,70 +6,94 @@ set -o xtrace
 TMP=/var/tmp
 AGENTS="$(ls *.tgz *.tar.gz || /bin/true)"
 AGENTS_DIR=/opt/smartdc/agents
+export PATH="$PATH:$AGENTS_DIR/bin"
 
 export PATH=$AGENTS_DIR/modules/.npm/agents_core/active/package/local/bin:$PATH
 
 SDC_CONFIG=/lib/sdc/config.sh
 
 if [ -x "$SDC_CONFIG" ]; then
-    source $SDC_CONFIG
-    load_sdc_config
-    load_sdc_sysinfo
+  source $SDC_CONFIG
+  load_sdc_config
+  load_sdc_sysinfo
 fi
 
-npm-install() {
-    WHAT=$1
-    $AGENTS_DIR/bin/agents-npm --no-registry install "$WHAT"
+message() {
+  echo "==> $*" >&2
 }
 
-# Upgrade from Lime-era release.
-if [ -f /opt/smartdc/agents/bin/agents-npm ] && /opt/smartdc/agents/bin/agents-npm --no-registry ls atropos | grep 'installed';
-then
-    TOREMOVE="$(/opt/smartdc/agents/bin/agents-npm --no-registry ls installed)"
-    for agent in "$TOREMOVE"; do
-        if (echo "$agent" | grep '^atropos@'); then
-            continue
-        fi
+npm-install() {
+  WHAT=$1
+  agents-npm --no-registry install "$WHAT"
+}
 
-        /opt/smartdc/agents/bin/agents-npm uninstall $agent;
-    done
-    /opt/smartdc/agents/bin/agents-npm uninstall atropos
+rm-agent-dirs() {
+  for dir in $(ls "$AGENTS_DIR" | grep -v "^db$"); do
+    if [ "$dir" == "smf" ]; then
+      rm $AGENTS_DIR/$dir/*
+    else
+      rm -fr $AGENTS_DIR/$dir
+    fi
+  done
+}
 
-    rm /opt/smartdc/agents/bin/agents-npm
-    for dir in $(ls /opt/smartdc/agents/ | grep -v "^db$"); do
-        rm -fr $AGENTS_DIR/$dir
-    done
-elif [ -f /opt/smartdc/agents/bin/agents-npm ]; then
-    TOREMOVE="$(/opt/smartdc/agents/bin/agents-npm --no-registry ls installed)"
-    for agent in "$TOREMOVE"; do
-        if (echo "$agent" | grep '^atropos@'); then
-            continue
-        fi
+cleanup-lime() {
+  message "Upgrading agents from Lime-era release."
+  TOREMOVE="$(agents-npm --no-registry ls installed | grep -v '^atropos@') atropos"
+  for agent in "$TOREMOVE"; do
+    log "Attempting to uninstall $agent"
+    agents-npm uninstall $agent;
+  done
 
-        /opt/smartdc/agents/bin/agents-npm uninstall $agent;
-    done
-    for dir in $(ls /opt/smartdc/agents/ | grep -v "^db$"); do
-        rm -fr $AGENTS_DIR/$dir
-    done
-    rm /opt/smartdc/agents/bin/agents-npm
-fi
+  rm-agent-dirs
+}
 
-# Run the bootstrap script
-if [ ! -f $AGENTS_DIR/bin/agents-npm ] || $AGENTS_DIR/bin/agents-npm --no-registry ls agents_core | grep 'installed'; then
-  # Install the actual atropos agent
-  tar -zxvf agents_core-*.tgz
-  (cd agents_core && ./bootstrap/bootstrap.sh "$AGENTS_DIR")
-fi
+cleanup-agents() {
+  message "Updating existing agents install."
+  TOREMOVE="$(agents-npm --no-registry ls installed | awk '{ print $1 }')"
+  for agent in "$TOREMOVE"; do
+    if (echo "$agent" | grep '^atropos@'); then 
+      continue
+    fi
 
-# Install the agents locally
-for tarball in $AGENTS; do
+    agents-npm uninstall $agent;
+  done
+
+  rm-agent-dirs
+}
+
+cleanup-existing() {
+  if [ -f "$AGENTS_DIR/agents-npm" ] && agents-npm --no-registry ls atropos | grep 'installed'; then
+    cleanup-lime
+  elif [ -f "$AGENTS_DIR/agents-npm" ]; then
+    cleanup-agents
+  fi
+}
+
+bootstrap() {
+  # Run the bootstrap script
+  if [ ! -f $AGENTS_DIR/bin/agents-npm ] || $AGENTS_DIR/bin/agents-npm --no-registry ls agents_core | awk '{ print $1 }' | grep 'installed'; then
+    # Install the actual atropos agent
+    tar -zxvf agents_core-*.tgz
+    (cd agents_core && ./bootstrap/bootstrap.sh "$AGENTS_DIR")
+  fi
+}
+
+install-agents() {
+  # Install the agents locally
+  for tarball in $AGENTS; do
     case "$tarball" in
-        agents_core-*.tgz)
-            ;;
-        *)
-            npm-install "./$tarball"
-            ;;
+      agents_core-*.tgz)
+        ;;
+      *)
+        npm-install "./$tarball"
+        ;;
     esac
-done
+  done
+}
+
+cleanup-existing
+bootstrap
+install-agents
 
 exit 0
